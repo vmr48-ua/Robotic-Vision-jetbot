a8188a6d796b4667824ac1edcd9ccff575dc30b3
vmr48-ua <78732677+vmr48-ua@users.noreply.github.com>
2024-03-23 20:27:22 +0100

    code style modifications

--- server.py
+++ server.py
@@ -1,31 +1,24 @@
 import socket # networking lib
 import jetbot
+import atexit
 
-robot = jetbot.Robot()
-HOST = '192.168.28.187'
-PORT = '8080'
-
-print('Server Started')
-connections = []
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setblocking(0)
-server.bind((HOST, int(PORT))) # declare server
-server.listen(1) # max connection streams
-
-def close_socket(connection):
+@atexit.register
+def close_socket(connection) -> None:
     try:
         connection.shutdown(socket.SHUT_RDWR)
-    except:
-        pass
-    try:
         connection.close()
     except:
         pass
+    try:
+        robot.left_motor.value = 0
+        robot.right_motor.value = 0
+    except:
+        pass
     
-def read():
+def read(connections, packet_size: int = 22) -> bytes:
     for i in reversed(range(len(connections))):
         try:
-            data, sender = connections[i][0].recvfrom(22)
+            data, sender = connections[i][0].recvfrom(packet_size)
             return data
         except (BlockingIOError, socket.timeout, OSError):
             pass
@@ -34,7 +27,7 @@ def read():
             connections.pop(i)
     return b''  # return empty if no data found
             
-def decode(packet):
+def decode(packet: bytes) -> (float,float):
     output = packet.decode()
     output = output.split(',')
     x = output[0].split('(')[1]
@@ -42,29 +35,44 @@ def decode(packet):
     x = x.split("'")[1]
     y = y.split("'")[1]
     return float(x),float(y)
-        
-running = True
-while running:
-    try:
-        con, addr = server.accept()
-        connections.append((con, addr))
-    except BlockingIOError:
-        pass
 
-    data = read()
-    if data != b'':
-        if 'exit' in data.decode():
-            running = False
-            break
-        if 'camera' in data.decode():
-            #take a pic
-            break
-        motorL, motorR = decode(data)
-        robot.left_motor.value = motorL
-        robot.right_motor.value = motorR
+def main() -> None:
+    robot = jetbot.Robot()
+    HOST = '192.168.28.187'
+    PORT = '8080'
+    connections = []
+    running: bool = True
 
-# Close the sockets
-for i in reversed(range(len(connections))):
-    close_socket(connections[i][0])
-    connections.pop(i)
-close_socket(server)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setblocking(0)
+    server.bind((HOST, int(PORT))) # declare server
+    server.listen(1) # max connection streams
+    print('Server Started')
+
+    while running:
+        try:
+            con, addr = server.accept()
+            connections.append((con, addr))
+        except BlockingIOError:
+            pass
+
+        data: bytes = read(connections)
+        if data != b'':
+            if 'exit' in data.decode():
+                running = False
+                break
+            if 'camera' in data.decode():
+                #take a pic
+                break
+            motorL, motorR = decode(data)
+            robot.left_motor.value = motorL
+            robot.right_motor.value = motorR
+
+    # Close the sockets
+    for i in reversed(range(len(connections))):
+        close_socket(connections[i][0])
+        connections.pop(i)
+    close_socket(server)
+
+if __name__ == '__main__':
+    main()
